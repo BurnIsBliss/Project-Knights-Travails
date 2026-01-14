b7a388272b43f0c2f7f3bb43dabb0f41085a848c
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2026-01-15 00:50:31 +0530

    Refactored the listCreation function to make the graph using all possible Knight moves from each cell/square/vertex.

--- main.js
+++ main.js
@@ -9,10 +9,14 @@ class GraphPoints {
 		for (let i = 0; i < 8; i++) {
 			for (let j = 0; j < 8; j++) {
 				let tempArray = [];
-				if (i - 1 >= 0) tempArray.push(i - 1, j);
-				if (i + 1 <= 7) tempArray.push(i + 1, j);
-				if (j - 1 >= 0) tempArray.push(i, j - 1);
-				if (j + 1 <= 7) tempArray.push(i, j + 1);
+				if (i - 1 >= 0 && j + 2 <= 7) tempArray.push(i - 1, j + 2);
+				if (i - 1 >= 0 && j - 2 >= 0) tempArray.push(i - 1, j - 2);
+				if (i + 1 <= 7 && j + 2 <= 7) tempArray.push(i + 1, j + 2);
+				if (i + 1 <= 7 && j - 2 >= 0) tempArray.push(i + 1, j - 2);
+				if (j - 1 >= 0 && i - 2 >= 0) tempArray.push(i - 2, j - 1);
+				if (j - 1 >= 0 && i + 2 <= 7) tempArray.push(i + 2, j - 1);
+				if (j + 1 <= 7 && i - 2 >= 0) tempArray.push(i - 2, j + 1);
+				if (j + 1 <= 7 && i + 2 <= 7) tempArray.push(i + 2, j + 1);
 				mapObject.set([i, j], tempArray);
 			}
 		}
@@ -24,6 +28,8 @@ class GraphPoints {
 			console.log(`${key}: ${value}`);
 		}
 	}
+
+	knightMoves(start, end) {}
 }
 
 export { GraphPoints };
